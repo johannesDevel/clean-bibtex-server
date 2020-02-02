23aabaac87019b221d36e30ea455587d2907826c
johannes <e1346716@student.tuwien.ac.at>
2020-02-02 13:34:59 +0100

    implemented searching for misspelled author names

--- src/author/checkAuthor.js
+++ src/author/checkAuthor.js
@@ -7,7 +7,9 @@ const splitAuthor = (author, entries) =>
           name: name,
           abbreviated: checkAbbreviation(getFirstName(name)),
           changedAbbreviation: false,
-          misspelling: false
+          misspelling: false,
+          changedMisspelling: false,
+          suggestion: []
         }))
     : null;
 
@@ -42,11 +44,96 @@ const searchSuggestion = (abbreviatedName, entries) =>
     );
 
 const searchDuplicatedAuthors = entries => {
-  const allAuthors = entries.filter(
-    entry => entry.AUTHOR != null && entry.AUTHOR.length > 0
-  ).flatMap(entry => entry.AUTHOR.map(author => author));
+  const allAuthors = entries
+    .filter(entry => entry.AUTHOR != null && entry.AUTHOR.length > 0)
+    .flatMap(entry =>
+      entry.AUTHOR.map(author => ({ ...author, entryId: entry.id }))
+    );
+  const foundSimilarAuthors = [];
 
-  console.log(allAuthors);
+  allAuthors.forEach(currentAuthor => {
+    allAuthors.forEach(author => {
+      if (
+        getEditDistance(author.name, currentAuthor.name) <= 2 &&
+        getEditDistance(author.name, currentAuthor.name) > 0 &&
+        author.entryId !== currentAuthor.entryId
+      ) {
+        if (
+          !foundSimilarAuthors.some(
+            foundAuthor =>
+              foundAuthor.firstName === currentAuthor.name &&
+              foundAuthor.secondName === author.name &&
+              foundAuthor.firstEntryId === currentAuthor.entryId &&
+              foundAuthor.secondEntryId === author.entryId
+          )
+        ) {
+          foundSimilarAuthors.push({
+            firstName: author.name,
+            secondName: currentAuthor.name,
+            firstEntryId: author.entryId,
+            secondEntryId: currentAuthor.entryId
+          });
+        }
+      }
+    });
+  });
+
+  foundSimilarAuthors.forEach(foundAuthorPair => {
+    entries.forEach(entry => {
+      if (entry.id === foundAuthorPair.firstEntryId) {
+        const foundMisspelledAuthor = entry.AUTHOR.find(
+          author => author.name === foundAuthorPair.firstName
+        );
+        foundMisspelledAuthor.suggestion.unshift(foundAuthorPair.secondName);
+        foundMisspelledAuthor.misspelling = true;
+      }
+      if (entry.id === foundAuthorPair.secondEntryId) {
+        const foundMisspelledAuthor = entry.AUTHOR.find(
+          author => author.name === foundAuthorPair.secondName
+        );
+        foundMisspelledAuthor.suggestion.unshift(foundAuthorPair.firstName);
+        foundMisspelledAuthor.misspelling = true;
+      }
+    });
+  });
+};
+
+const getEditDistance = (word1, word2) => {
+  const len1 = word1.length;
+  const len2 = word2.length;
+  const dp = new Array(len1 + 1);
+
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(len2 + 1);
+  }
+  for (let i = 0; i <= len1; i++) {
+    dp[i][0] = i;
+  }
+  for (let j = 0; j <= len2; j++) {
+    dp[0][j] = j;
+  }
+  // iterate though, and check last char
+  for (let i = 0; i < len1; i++) {
+    const c1 = word1.charAt(i);
+    for (let j = 0; j < len2; j++) {
+      const c2 = word2.charAt(j);
+
+      // if last two chars equal
+      if (c1 === c2) {
+        // update dp value for +1 length
+        dp[i + 1][j + 1] = dp[i][j];
+      } else {
+        const replaceVal = dp[i][j] + 1;
+        const insertVal = dp[i][j + 1] + 1;
+        const deleteVal = dp[i + 1][j] + 1;
+
+        let min = replaceVal > insertVal ? insertVal : replaceVal;
+        min = deleteVal > min ? min : deleteVal;
+        dp[i + 1][j + 1] = min;
+      }
+    }
+  }
+  return dp[len1][len2];
 };
 
 const compareFirstLetter = (name1, name2) =>
