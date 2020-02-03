977333338075011d406c37b2b5b2a12c78a3c2ab
johannes <e1346716@student.tuwien.ac.at>
2020-02-03 09:17:25 +0100

    build client

---
+++ client/build/precache-manifest.dc5f3b6ad9d05071e0365b812a453997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18087235a53fcd8e036d7044f6f50c8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0afd408eba3f0ec67327",
+    "url": "/static/css/main.440e6289.chunk.css"
+  },
+  {
+    "revision": "db6c740a2ca8cd02c61c",
+    "url": "/static/js/2.c11375af.chunk.js"
+  },
+  {
+    "revision": "0afd408eba3f0ec67327",
+    "url": "/static/js/main.41a27df3.chunk.js"
+  },
+  {
+    "revision": "4b44a20f956fcb9e5bae",
+    "url": "/static/js/runtime~main.706cd02e.js"
+  }
+]);
