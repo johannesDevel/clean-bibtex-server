1317bdaf62b41f14b024a3ae468a54f52a399831
johannes <e1346716@student.tuwien.ac.at>
2020-02-26 15:34:14 +0100

    fixed correction of capitalization hyphen-connected-words

--- src/capitalization/correctEntries.js
+++ src/capitalization/correctEntries.js
@@ -1,11 +1,21 @@
 const getCorrectedTitleCaseTitle = title => {
-  const correctedTitleCase = correctToTitleCase(title);
+  const correctedTitleCase = splitTitle(title).map(word =>
+    correctWordTitleCase(word)
+  );
   return correctCapitalBeginning(correctedTitleCase);
 };
 
 const getCorrectedSentenceCaseTitle = title => {
-  const correctedSentenceCase = correctToSentenceCase(title);
+  const correctedSentenceCase = splitTitle(title).map(word =>
+    correctWordSetenceCase(word)
+  );
   return correctCapitalBeginning(correctedSentenceCase);
+};
+
+const splitTitle = title => {
+  const cleanedTitle = title.replace(/[\s]+/g, " ");
+  const titleArray = cleanedTitle.split(/[\s]+/);
+  return titleArray;
 };
 
 const correctCapitalBeginning = title => {
@@ -25,6 +35,15 @@ const correctWordTitleCase = word => {
   if (word === word.toUpperCase()) {
     return word;
   }
+  if (word.includes("-")) {
+    for (let i = 0; i < word.length - 1; i++) {
+      const currentLetter = word[i];
+      const previousLetter = word[i - 1];
+      if (i > 0 && previousLetter === "-" && currentLetter !== currentLetter.toUpperCase()) {
+        word = word.substr(0, i) + currentLetter.toUpperCase() + word.substr(i + 1);
+      }
+    }
+  }
   const firstLetter = word[0];
 
   let correctedWord = word;
@@ -41,35 +60,21 @@ const correctWordSetenceCase = word => {
   if (word === word.toUpperCase()) {
     return word;
   }
+  if (word.includes("-")) {
+    for (let i = 0; i < word.length - 1; i++) {
+      const currentLetter = word[i];
+      const previousLetter = word[i - 1];
+      if (i > 0 && previousLetter === "-" && currentLetter !== currentLetter.toLowerCase()) {
+        word = word.substr(0, i) + currentLetter.toLowerCase() + word.substr(i + 1);
+      }
+    }
+  }
+
   const correctedWord = word.charAt(0).toLowerCase() + word.slice(1);
   return correctedWord;
 };
 
-const correctToTitleCase = title => {
-  const cleanedTitle = title.replace(/[\s-]+/g, " ");
-  const titleArray = cleanedTitle.split(/[ -]+/);
-
-  const firstWord = titleArray[0];
-  if (firstWord[0] !== firstWord[0].toUpperCase()) {
-    firstWord[0] = firstWord[0].toUpperCase();
-  }
-  return titleArray.map(word => correctWordTitleCase(word));
-};
-
-const correctToSentenceCase = title => {
-  const cleanedTitle = title.replace(/[\s-]+/g, " ");
-  const titleArray = cleanedTitle.split(/[ -]+/);
-
-  const firstWord = titleArray[0];
-  if (firstWord[0] !== firstWord[0].toUpperCase()) {
-    firstWord[0] = firstWord[0].toUpperCase();
-  }
-  return titleArray.map(word => correctWordSetenceCase(word));
-};
-
 module.exports = {
-  correctToTitleCase,
-  correctToSentenceCase,
   getCorrectedTitleCaseTitle,
   getCorrectedSentenceCaseTitle
 };
